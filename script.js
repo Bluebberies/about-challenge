8b0dd8f1c52de6d812c01d5ef36e0192f712b997
Francis <francisalexander000@gmail.com>
2022-06-06 17:46:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    setTimeout( function() {
+        loader.style.display = "none";
+    }, 1000 )
+})
